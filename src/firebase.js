f65550bba98a177a0312fc6dd138ef2ed7fadeb5
Beknaz Baktygulov <enelbbs9@gmail.com>
2021-05-18 11:54:28 -0700

    Initial upload

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBmoH8Ajf_r0pEja31v3g-jD27L02VOpJI",
+    authDomain: "slack-6bf74.firebaseapp.com",
+    projectId: "slack-6bf74",
+    storageBucket: "slack-6bf74.appspot.com",
+    messagingSenderId: "667866725254",
+    appId: "1:667866725254:web:83b2139978b072d3c3ca07"
+  };
