5a3cabc9719e0d2f7efd17f58427c371871a966a
Beknaz Baktygulov <enelbbs9@gmail.com>
2021-05-18 12:12:25 -0700

    Installed react-router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import React from 'react';
-
 import './App.css';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
 function App() {
   return (
     <div className="app">
-      <h1>Hello Slackers</h1>
+      
+      <Router>
+      <>
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/users">
+            <Users />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </>
+    </Router>
     </div>
   );
 }
