6e0f9f6a32a2b173394d081475686809fc0cafa2
Beknaz Baktygulov <enelbbs9@gmail.com>
2021-05-21 14:15:37 -0700

    Created firebase database

--- src/firebase.js
+++ src/firebase.js
@@ -11,4 +11,6 @@ const firebaseConfig = {
   };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig)
-const db = 
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
