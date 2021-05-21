8915c70e34296c1ac9352dd5fe1c22650bde19fd
Beknaz Baktygulov <enelbbs9@gmail.com>
2021-05-21 13:05:11 -0700

    Developed sidebar info

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import Header from './Header';
 import styled from "styled-components";
+import Sidebar from './Sidebar';
 
 function App() {
   return (
@@ -29,5 +30,6 @@ export default App;
 
 
 const AppBody = styled.div`
-
+  display: flex;
+  height: 100vh;
 `;
