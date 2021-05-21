2c16c5b942c8f8d19bc9c1cde699436089fc3849
Beknaz Baktygulov <enelbbs9@gmail.com>
2021-05-21 14:09:54 -0700

    I've brought in firebase

--- src/SidebarOption.js
+++ src/SidebarOption.js
@@ -3,7 +3,20 @@ import styled from "styled-components";
 
 
 function SidebarOption({ title, Icon, addChannelOption }) {
-    return <SidebarOptionContainer>
+
+    const addChannel = () => {
+        
+    };
+
+
+    const selectChannel = () => {
+
+    }
+
+
+    return <SidebarOptionContainer
+        onClick={addChannelOption ? addChannel : selectChannel}
+    >
         {Icon && <Icon fontSize='small' style={{ padding: 10 }} /> }
         {Icon ? (
             <h3>{title}</h3>
--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,6 @@
+import firebase from 'firebase';
+
+
 const firebaseConfig = {
     apiKey: "AIzaSyBmoH8Ajf_r0pEja31v3g-jD27L02VOpJI",
     authDomain: "slack-6bf74.firebaseapp.com",
@@ -6,3 +9,6 @@ const firebaseConfig = {
     messagingSenderId: "667866725254",
     appId: "1:667866725254:web:83b2139978b072d3c3ca07"
   };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig)
+const db = 
