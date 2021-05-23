0d9b65607d8f11e528f2a384412466395988601d
Beknaz Baktygulov <enelbbs9@gmail.com>
2021-05-23 11:54:03 -0700

    Styled chat container

--- src/Chat.js
+++ src/Chat.js
@@ -1,17 +1,69 @@
 import styled from "styled-components";
 import React from 'react'
+import StarBorderIcon from '@material-ui/icons/StarBorder';
+import InfoIcon from '@material-ui/icons/Info';
 
 function Chat() {
     return (
         <ChatContainer>
             <h2>I am the chat screen</h2>
-            
+            <Header>
+                <HeaderLeft>
+                    <h4><strong>#Room name</strong></h4>
+                    <StarBorderIcon />
+                </HeaderLeft>
+                <HeaderRight>
+                    <p>
+                        <InfoIcon /> Details 
+                    </p>
+
+                </HeaderRight>
+
+            </Header>
             
         </ChatContainer>
     )
 }
 
 export default Chat
+
+const Header = styled.div`
+    display: flex;
+    justify-content: space-between;
+    padding: 20px;
+    border-bottom: 1px solid lightgray;
+`;
+
+const HeaderLeft = styled.div`
+
+    display: flex;
+    align-items: center;
+
+    > h4 {
+        display: flex;
+        text-transform: lowercase;
+        margin-right: 10px;
+    }
+
+    > h4 > .MuiSvgIcon-root {
+        margin-left: 20px;
+        font-size: 10px;
+    }
+`;
+
+const HeaderRight = styled.div`
+    > p {
+        display: flex;
+        align-items: center;
+        font-size: 14px;
+    }
+
+    > p > .MuiSvgIcon-root {
+        margin-right: 5px !important;
+        font-size: 16px;
+    }
+
+`;
 
 
 const ChatContainer = styled.div`
