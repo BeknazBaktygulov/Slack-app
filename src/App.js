da8bda2e946f06411e72b26515f8e743a9cdf351
Beknaz Baktygulov <enelbbs9@gmail.com>
2021-05-24 16:00:51 -0700

    Developed spinner and react spinkit

--- src/App.js
+++ src/App.js
@@ -2,15 +2,27 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Header from './Header';
-import styled from "styled-components";
 import Sidebar from './Sidebar';
 import Chat from './Chat';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { auth } from './firebase';
 import Login from './Login';
+import styled from "styled-components";
+import Spinner from 'react-spinkit';
 
 function App() {
   const [user, loading] = useAuthState(auth);
+
+  if (loading) {
+    return (
+      <AppLoading>
+          <AppLoadingContents>
+              <img src="https://cdn.dribbble.com/users/1238764/screenshots/5864612/slack-animation.gif" alt="" />
+
+          </AppLoadingContents>
+      </AppLoading>
+    )
+  }
 
   return (
     <div className="app">
@@ -37,6 +49,14 @@ function App() {
 }
 
 export default App;
+
+const AppLoading = styled.div`
+
+`;
+
+const AppLoadingContents = styled.div`
+  
+`;
 
 
 const AppBody = styled.div`
